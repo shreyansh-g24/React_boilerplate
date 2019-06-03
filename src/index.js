99edb19b51f54c6c591f9afad7136992c55f43ad
SG24 <shreyanshgupta98@gmail.com>
2019-06-03 12:21:54 +0530

    added js files

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// importing modules
+import add from "./add";
+import { sub } from "./sub";
+
+// executing code
+console.log("Adding 24 and 34: ", add(24, 34));
+console.log("Subtracting 24 from 34: ", sub(24, 34)); 
---
+++ src/sub.js
@@ -0,0 +1,3 @@
+export const sub = (a, b) => {
+  return b - a;
+}
